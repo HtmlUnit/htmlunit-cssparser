94f7caa25137b4cc3c3e068012df0c7daa6ccc2b
rbri <rbri@rbri.de>
2018-02-21 20:06:20 +0100

    % cleanup

--- src/main/java/com/gargoylesoftware/css/dom/DOMExceptionImpl.java
+++ src/main/java/com/gargoylesoftware/css/dom/DOMExceptionImpl.java
@@ -14,9 +14,6 @@
  */
 package com.gargoylesoftware.css.dom;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
 import org.w3c.dom.DOMException;
 
 /**
@@ -29,29 +26,46 @@ public class DOMExceptionImpl extends DOMException {
     public static final int SYNTAX_ERROR = 0;
     public static final int INDEX_OUT_OF_BOUNDS = 1;
     public static final int READ_ONLY_STYLE_SHEET = 2;
-    public static final int EXPECTING_UNKNOWN_RULE = 3;
-    public static final int EXPECTING_STYLE_RULE = 4;
-    public static final int EXPECTING_CHARSET_RULE = 5;
-    public static final int EXPECTING_IMPORT_RULE = 6;
-    public static final int EXPECTING_MEDIA_RULE = 7;
-    public static final int EXPECTING_FONT_FACE_RULE = 8;
-    public static final int EXPECTING_PAGE_RULE = 9;
-    public static final int FLOAT_ERROR = 10;
-    public static final int STRING_ERROR = 11;
-    public static final int COUNTER_ERROR = 12;
-    public static final int RECT_ERROR = 13;
-    public static final int RGBCOLOR_ERROR = 14;
-    public static final int CHARSET_NOT_FIRST = 15;
-    public static final int CHARSET_NOT_UNIQUE = 16;
-    public static final int IMPORT_NOT_FIRST = 17;
-    public static final int NOT_FOUND = 18;
-    public static final int NOT_IMPLEMENTED = 19;
-    public static final int INSERT_BEFORE_IMPORT = 20;
+    public static final int EXPECTING_STYLE_RULE = 3;
+    public static final int EXPECTING_CHARSET_RULE = 4;
+    public static final int EXPECTING_IMPORT_RULE = 5;
+    public static final int EXPECTING_MEDIA_RULE = 6;
+    public static final int EXPECTING_FONT_FACE_RULE = 7;
+    public static final int EXPECTING_PAGE_RULE = 8;
+    public static final int FLOAT_ERROR = 9;
+    public static final int STRING_ERROR = 10;
+    public static final int COUNTER_ERROR = 11;
+    public static final int RECT_ERROR = 12;
+    public static final int RGBCOLOR_ERROR = 13;
+    public static final int CHARSET_NOT_FIRST = 14;
+    public static final int CHARSET_NOT_UNIQUE = 15;
+    public static final int IMPORT_NOT_FIRST = 16;
+    public static final int NOT_FOUND = 17;
+    public static final int NOT_IMPLEMENTED = 18;
+    public static final int INSERT_BEFORE_IMPORT = 19;
 
-    private static ResourceBundle ExceptionResource_ =
-        ResourceBundle.getBundle(
-            "com.gargoylesoftware.css.parser.ExceptionResource",
-            Locale.ENGLISH);
+    static final String[] messages = {
+        "Syntax error",
+        "Index out of bounds error",
+        "This style sheet is read only",
+        "The text does not represent a style rule",
+        "The text does not represent a charset rule",
+        "The text does not represent an import rule",
+        "The text does not represent a media rule",
+        "The text does not represent a font face rule",
+        "The text does not represent a page rule",
+        "This isn't a Float type",
+        "This isn't a String type",
+        "This isn't a Counter type",
+        "This isn't a Rect type",
+        "This isn't an RGBColor type",
+        "A charset rule must be the first rule",
+        "A charset rule already exists",
+        "An import rule must preceed all other rules",
+        "The specified type was not found",
+        "The functionality is not implemented",
+        "Can't insert a rule before the last charset or import rule"
+    };
 
     public DOMExceptionImpl(final short code, final int messageKey) {
         this(code, messageKey, null);
@@ -62,15 +76,6 @@ public class DOMExceptionImpl extends DOMException {
     }
 
     public DOMExceptionImpl(final int code, final int messageKey, final String info) {
-        super((short) code, constructMessage(messageKey, info));
-    }
-
-    private static String constructMessage(final int key, final String info) {
-        final String messageKey = "s" + String.valueOf(key);
-        String message = ExceptionResource_.getString(messageKey);
-        if (null != info && info.length() > 0) {
-            message = message + " (" + info + ")";
-        }
-        return message;
+        super((short) code, messages[messageKey] + " (" + info + ")");
     }
 }
